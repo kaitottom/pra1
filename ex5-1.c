ac81c0ecc910fc12799269693baaea59b76a1601
kaito <r202203438rx@jindai.jp>
2023-12-19 21:43:21 +0900

    second version

--- ex5-1.c
+++ ex5-1.c
@@ -4,7 +4,7 @@
 int main(void) {
   char *greet1 = "What's up?", *greet2 = "Business as usual!";
 
-  printf("Hi mate. %s %s/n", greet1, greet2);
+  printf("Hi mate. %s %s\n", greet1, greet2);
 
   return 0;
 }
