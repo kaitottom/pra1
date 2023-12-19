28edbf11f812fde8b7b61c9cdc57868a207cbd5b
kaito <r202203438rx@jindai.jp>
2023-12-19 21:17:36 +0900

    initial version

--- ex5-1.c
+++ ex5-1.c
@@ -1,17 +1,3 @@
-/*echo "# se2023ex5" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/kaitottom/se2023ex5.git
-git push -u origin main
-*/
-
-/*git remote add origin https://github.com/kaitottom/se2023ex5.git
-git branch -M main
-git push -u origin main
-*/
-
 #include <stdio.h>
 #include <string.h>
 
