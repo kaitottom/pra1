4526d53f9c3671e51a52679688d55e5ec31acab7
kaito <r202203438rx@jindai.jp>
2023-12-19 21:55:17 +0900

    third version

--- ex5-1.c
+++ ex5-1.c
@@ -2,9 +2,14 @@
 #include <string.h>
 
 int main(void) {
+  int wcounter = 0;
   char *greet1 = "What's up?", *greet2 = "Business as usual!";
 
   printf("Hi mate. %s %s\n", greet1, greet2);
+
+  wcounter = strlen("Hi mate.");
+  wcounter += strlen(greet1) + strlen(greet2);
+  printf("the numer of alphabets is %d\n", wcounter);
 
   return 0;
 }
